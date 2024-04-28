64e733aa0c3bf6aeff5222e553ec5dbf1baeed41
Peerapat Ngamgsanga <6410685223@cn331>
2024-04-28 14:12:31 +0700

    check

---
+++ go_practice/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	target := "https://quizlet.com"
+	resp, err := http.Get(target)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Server software:", resp.Header.Get("Server"))
+	if strings.Contains(target, "http://") {
+		target = strings.TrimPrefix(target, "http://")
+	} else if strings.Contains(target, "https://") {
+		target = strings.TrimPrefix(target, "https://")
+	}
+	fmt.Println("Updated target URL:", target)
+	cmd := exec.Command("nmap", "-O", target)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error executing Nmap command:", err)
+		return
+	}
+
+	outputStr := string(output)
+	lines := strings.Split(outputStr, "\n")
+
+	var cpeLine, osGuessLine string
+
+	for i := 0; i < len(lines); i++ {
+		line := strings.TrimSpace(lines[i])
+		if strings.Contains(line, "OS CPE:") {
+			cpeLine = line[len("OS CPE:"):]
+			// print(line[len("OS CPE:"):])
+		} else if strings.Contains(line, "Aggressive OS guesses:") {
+			osGuessLine = line[len("Aggressive OS guesses:"):]
+		}
+	}
+
+	// Print the desired lines
+	if cpeLine == "" && osGuessLine == "" {
+		fmt.Println("OS Detection: Not Found")
+	} else {
+		fmt.Println("OS detection: ", cpeLine)
+		fmt.Println("OS Guess: ", osGuessLine)
+	}
+}
