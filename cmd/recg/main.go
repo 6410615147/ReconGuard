eee2a579c40e2f8412ba512729d332673ccc38c2
Peerapat Ngamgsanga <6410685223@cn331>
2024-04-29 22:48:40 +0700

    merge

--- cmd/recg/main.go
+++ cmd/recg/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"os/exec"
 	"recg/internal/scanner"
+	"strings"
 )
 
 func main() {
-
 	fmt.Println("Welcome to ReconGuard")
 
 	// Define flags for command-line options
@@ -15,10 +17,16 @@ func main() {
 	portRange := flag.String("p", "1-1023", "Port range to scan (e.g., '80' or '80-100')")
 	commonPort := flag.Bool("cp", false, "Scan common ports")
 
+	// Define flag for web server reconnaissance
+	targetFlag := flag.String("o", "", "Target website URL")
+
 	// Parse command-line flags
 	flag.Parse()
 
-	// Create a map to associate flags with functions
+	// Perform web server reconnaissance if -o flag is provided
+	if *targetFlag != "" {
+		performWebServerReconnaissance(*targetFlag)
+	}
 	flagHandlers := map[*flag.Flag]func(){
 		flag.Lookup("p"): func() { port_flag(*targetHost, *portRange) },
 	}
@@ -34,6 +42,51 @@ func main() {
 	})
 }
 
+func performWebServerReconnaissance(target string) {
+	resp, err := http.Get(target)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Server software:", resp.Header.Get("Server"))
+	if strings.Contains(target, "http://") {
+		target = strings.TrimPrefix(target, "http://")
+	} else if strings.Contains(target, "https://") {
+		target = strings.TrimPrefix(target, "https://")
+	}
+	fmt.Println("Updated target URL:", target)
+	cmd := exec.Command("nmap", "-O", target)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error executing Nmap command:", err)
+		return
+	}
+
+	outputStr := string(output)
+	lines := strings.Split(outputStr, "\n")
+
+	var cpeLine, osGuessLine string
+
+	for i := 0; i < len(lines); i++ {
+		line := strings.TrimSpace(lines[i])
+		if strings.Contains(line, "OS CPE:") {
+			cpeLine = line[len("OS CPE:"):]
+		} else if strings.Contains(line, "Aggressive OS guesses:") {
+			osGuessLine = line[len("Aggressive OS guesses:"):]
+		}
+	}
+
+	// Print the desired lines
+	if cpeLine == "" && osGuessLine == "" {
+		fmt.Println("OS Detection: Not Found")
+	} else {
+		fmt.Println("OS detection: ", cpeLine)
+		fmt.Println("OS Guess: ", osGuessLine)
+	}
+}
 func port_flag(target string, portRange string) {
 
 	// Parse the port range string
